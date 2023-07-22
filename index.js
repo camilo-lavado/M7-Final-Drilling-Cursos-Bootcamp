d6aa3c838f29134131df5ac2a0072e9ba6c1c447
Camilo Lavado <93832685+Kroszover@users.noreply.github.com>
2023-07-22 13:02:49 -0400

    se añaden archivos faltantes

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ async function conectarYCrear() {
   } catch (error) {
     console.error("No se pudo conectar a la base de datos:\n", error);
   } finally {
-    await sequelize.sync({ force: true });
+    await sequelize.sync({ force: true }); //alter true
     console.log("\nBase de datos sincronizada.\n");
   }
 }
